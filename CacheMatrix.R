d7aadc5f4a3e2bc290e3b6c8ddedf3dc94135ec7
drsudhirpathak <32571627+drsudhirpathak@users.noreply.github.com>
2017-10-06 18:37:23 +0530

    Create CacheMatrix.R

---
+++ CacheMatrix.R
@@ -0,0 +1,22 @@
+makeCacheMatrix <- function(x = matrix()) {
+  ## @x: a square invertible matrix
+  ## return: a list containing functions to
+  ##              1. set the matrix
+  ##              2. get the matrix
+  ##              3. set the inverse
+  ##              4. get the inverse
+  ##         this list is used as the input to cacheSolve()
+  
+  inv = NULL
+  set = function(y) {
+    # use `<<-` to assign a value to an object in an environment 
+    # different from the current environment. 
+    x <<- y
+    inv <<- NULL
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
