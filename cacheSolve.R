33f4aaad19c4e891726dd728b49657c1d18819c3
drsudhirpathak <32571627+drsudhirpathak@users.noreply.github.com>
2017-10-06 18:38:31 +0530

    Create cacheSolve.R

---
+++ cacheSolve.R
@@ -0,0 +1,22 @@
+cacheSolve <- function(x, ...) {
+  ## @x: output of makeCacheMatrix()
+  ## return: inverse of the original matrix input to makeCacheMatrix()
+  
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    # get it from the cache and skips the computation. 
+    message("getting cached data")
+    return(inv)
+  }
+  
+  # otherwise, calculates the inverse 
+  mat.data = x$get()
+  inv = solve(mat.data, ...)
+  
+  # sets the value of the inverse in the cache via the setinv function.
+  x$setinv(inv)
+  
+  return(inv)
+}
